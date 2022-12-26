769e9e909d5795a0c1ec85744b3ead05e179a217
YeLinnAungGautam <davidgautam.1234@gmail.com>
2022-12-27 00:11:21 +0630

    WEbhokk code added

--- routes/webhook_verify.js
+++ routes/webhook_verify.js
@@ -1,33 +1,7 @@
-const processPostback = require('../processes/postback');
-const processMessage = require('../processes/messages');
-module.exports = function(app, chalk){
-  app.get('/webhook', function(req, res) {
-    if (req.query['hub.verify_token'] === "DEV_YELINNAUNG123"){
-       console.log('webhook verified');
-       res.status(200).send(req.query['hub.challenge']);
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
     } else {
-        console.log('verification failed. Token mismatch.');
-        // res.sendStatus(403);
-     }
-  });
-  
-  app.post('/webhook', function(req, res) {
-    //checking for page subscription.
-    if (req.body.object === 'page'){
-       /* Iterate over each entry, there can be multiple entries 
-       if callbacks are batched. */
-       req.body.entry.forEach(function(entry) {
-       // Iterate over each messaging event
-          entry.messaging.forEach(function(event) {
-          console.log(event);
-          if (event.postback){
-             processPostback(event);
-          } else if (event.message){
-             processMessage(event);
-          }
-      });
-    });
-    res.sendStatus(200);
-   }
-  });
-}
+        res.send('Invalid verify token');
+    }
+});  
