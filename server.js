18936a6ebc852a7609875ab3231a2813be75d6ba
YeLinnAungGautam <davidgautam.1234@gmail.com>
2022-12-30 14:20:11 +0630

    Latest Code

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ require('dotenv').config();
 const
   request = require('request'),
   express = require('express'),
-  bodyParser = require('body-parser'),
-  { text } = require('body-parser'),
+  { urlencoded, json } = require('body-parser'),
   app = express();
-  app.use(bodyParser.urlencoded({extended: false}));
-  app.use(bodyParser.json());
+
+// Parse application/x-www-form-urlencoded
+app.use(urlencoded({ extended: true }));
+
+// Parse application/json
+app.use(json());
 
 // Respond with 'Hello World' when a GET request is made to the homepage
 app.get('/', function (_req, res) {
@@ -47,139 +50,99 @@ app.get('/webhook', (req, res) => {
 });
 
 // Creates the endpoint for your webhook
-app.post('/webhook', function(req, res) {
-  // let body = req.body;
-  var event = req.body.entry[0].messaging;
-  for (i = 0; i < event.length; i++) {
-    var webhookEvent = event[i];
-    if (webhookEvent.message) {
-      if(!sendQuickReply(webhookEvent.sender.id, webhookEvent.message.receivedMessage)){
-        sendMessage(webhookEvent.sender.id);
+app.post('/webhook', (req, res) => {
+  let body = req.body;
+
+  // Checks if this is an event from a page subscription
+  if (body.object === 'page') {
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the body of the webhook event
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+
+      // Get the sender PSID
+      let senderPsid = webhookEvent.sender.id;
+      console.log('Sender PSID: ' + senderPsid);
+
+      // Check if the event is a message or postback and
+      // pass the event to the appropriate handler function
+      if (webhookEvent.message) {
+        handleMessage(senderPsid, webhookEvent.message);
+      } else if (webhookEvent.postback) {
+        handlePostback(senderPsid, webhookEvent.postback);
       }
-    }
-    if (webhookEvent.message) {
-      if(!sendReply(webhookEvent.sender.id, webhookEvent.message.receivedMessage)){
-        sendMessage(webhookEvent.sender.id);
-      }
-    }
-  }
+    });
     // Returns a '200 OK' response to all requests
     res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
 });
 
 // Handles messages events
-function sendQuickReply(recipientId, receivedMessage) {
-  receivedMessage = receivedMessage || "";
-  var values = receivedMessage.split();
-  if (values[0] === 'hi' || values[0] === 'Hi') {
-    message = {
-      text: "Choose Language",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"Myanmar",
-          "payload":"mm"
-        },
-        {
-          "content_type":"text",
-          "title":"English",
-          "payload":"eng"
-        },
-      ]
-    }
-    // Send the response message
-    sendMessage(recipientId, message);
-    return true;
-  }
-}
-function sendReply(recipientId, receivedMessage){
-  
-  receivedMessage = receivedMessage || "";
-  var values = receivedMessage.split();
-  if (values[0] === 'Myanmar' || values[0] === 'နောက်သို့') {
-    message= {
-      text: "Choose Your City",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"စွန့်စားမှုခရီး",
-          "payload":"advmm"
-        },
-        {
-          "content_type":"text",
-          "title":"ခရီးတို",
-          "payload":"shteng"
-        },
-      ]
-    }
-    // Send the response message
-    sendMessage(recipientId, message);
-    return true;
-  }
-}
-function sendMessage(recipientId, message) { 
-  request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-      method: 'POST',
-      json: {
-          recipient: {id: recipientId},
-          message: message,
-      }
-  }, 
-  function(error, response, body) {
-      if (error) {
-          console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-          console.log('Error: ', response.body.error);
-      }
-  });
-}; 
-// // Handles messaging_postbacks events
-// function handlePostback(senderPsid, receivedPostback) {
-//   let response;
+function handleMessage(senderPsid, receivedMessage) {
+  let response;
 
-//   // Get the payload for the postback
-//   let payload = receivedPostback.payload;
+  // Checks if the message contains text
+  if (receivedMessage.text) {
+    // Create the payload for a basic text message, which
+    // will be added to the body of your request to the Send API
+     response = {
+      'text': `Hello Welcome From Thailandanywhere`
+     };
+   } 
+  // Send the response message
+  callSendAPI(senderPsid, response);
+}
 
-//   // Set the response based on the postback payload
-//   if (payload === 'mm') {
-//     response = { 'text': 'Hello I am Burmese haha' };
-//   } else if (payload === 'eng') {
-//     response = { 'text': 'Hi I am english' };
-//   }
-//   // Send the message to acknowledge the postback
-//   callSendAPI(senderPsid, response);
-// }
+// Handles messaging_postbacks events
+function handlePostback(senderPsid, receivedPostback) {
+  let response;
+
+  // Get the payload for the postback
+  let payload = receivedPostback.payload;
+
+  // Set the response based on the postback payload
+  if (payload === 'mm') {
+    response = { 'text': 'Hello I am Burmese haha' };
+  } else if (payload === 'eng') {
+    response = { 'text': 'Hi I am english' };
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(senderPsid, response);
+}
 
 // Sends response messages via the Send API
-// function callSendAPI(senderPsid, response) {
+function callSendAPI(senderPsid, response) {
 
-//   // The page access token we have generated in your app settings
-//   const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+  // The page access token we have generated in your app settings
+  const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-//   // Construct the message body
-//   let requestBody = {
-//     'recipient': {
-//       'id': senderPsid
-//     },
-//     'message': response
-//   };
+  // Construct the message body
+  let requestBody = {
+    'recipient': {
+      'id': senderPsid
+    },
+    'message': response
+  };
 
-//   // Send the HTTP request to the Messenger Platform
-//   request({
-//     'uri': 'https://graph.facebook.com/v2.6/me/messages',
-//     'qs': { 'access_token': PAGE_ACCESS_TOKEN },
-//     'method': 'POST',
-//     'json': requestBody
-//   }, (err, _res, _body) => {
-//     if (!err) {
-//       console.log('Message sent!');
-//     } else {
-//       console.error('Unable to send message:' + err);
-//     }
-//   });
-// }
+  // Send the HTTP request to the Messenger Platform
+  request({
+    'uri': 'https://graph.facebook.com/v2.6/me/messages',
+    'qs': { 'access_token': PAGE_ACCESS_TOKEN },
+    'method': 'POST',
+    'json': requestBody
+  }, (err, _res, _body) => {
+    if (!err) {
+      console.log('Message sent!');
+    } else {
+      console.error('Unable to send message:' + err);
+    }
+  });
+}
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
