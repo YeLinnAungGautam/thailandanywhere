06dd45c148ef36fa3e3a4be52f8c5d62fb33ff30
YeLinnAungGautam <davidgautam.1234@gmail.com>
2023-01-05 13:22:12 +0630

    Code Updated

--- server.js
+++ server.js
@@ -132,6 +132,43 @@ function ChoosePackages(senderPsid,receivedMessage) {
   callSendAPI(senderPsid, response);
 //   return true;
 }
+function GroupTourPackage(senderPsid,receivedMessage){
+    let response;
+    if(receivedMessage.text === 'Kanchanaburi'){
+       response = {
+        "attachment":{
+            "type":"template",
+            "payload":{
+              "template_type":"generic",
+              "elements":[
+                 {
+                  "title":"Welcome!",
+                  "image_url":"https://raw.githubusercontent.com/fbsamples/original-coast-clothing/main/public/styles/male-work.jpg",
+                  "subtitle":"We have the right hat for everyone.",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://www.originalcoastclothing.com/",
+                    "webview_height_ratio": "tall"
+                  },
+                  "buttons":[
+                    {
+                      "type":"web_url",
+                      "url":"https://www.originalcoastclothing.com/",
+                      "title":"View Website"
+                    },{
+                      "type":"postback",
+                      "title":"Start Chatting",
+                      "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                    }              
+                  ]      
+                }
+              ]
+            }
+          }
+       }
+    }
+
+}
 
 // Handles messaging_postbacks events
 function handlePostback(senderPsid, receivedPostback) {
